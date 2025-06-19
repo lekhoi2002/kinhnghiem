bfcabd67e8ece7a7cd1ba33f8eea069fc6cdd789
lekhoi2002 <khoile2002@gmail.com>
2025-06-19 23:46:58 +0700

    fix theme v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,100 +1,70 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./layouts/**/*.{html,js}",
-    "./content/**/*.{md,html}",
-    "./assets/**/*.{js,css}",
-    "./static/**/*.{html,js}"
+    './layouts/**/*.{html,js}',
+    './content/**/*.{md,html}',
+    './assets/**/*.{js,css}',
+    './static/**/*.{html,js}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         warmgray: {
           700: '#2f3335',
           800: '#1e2022',
-          900: '#181a1b'
-        }
-      },
-      fontFamily: {
-        sans: ['system-ui', 'sans-serif']
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            maxWidth: 'none',
-            color: 'inherit',
-            a: {
-              color: 'inherit',
-              textDecoration: 'underline',
-              fontWeight: '500',
-            },
-            '[class~="lead"]': {
-              color: 'inherit',
-            },
-            strong: {
-              color: 'inherit',
-            },
-            'ol > li::before': {
-              color: 'inherit',
-            },
-            'ul > li::before': {
-              backgroundColor: 'currentColor',
-            },
-            hr: {
-              borderColor: 'currentColor',
-              opacity: '0.1',
-            },
-            blockquote: {
-              color: 'inherit',
-              borderLeftColor: 'currentColor',
-              opacity: '0.8',
-            },
-            h1: {
-              color: 'inherit',
-            },
-            h2: {
-              color: 'inherit',
-            },
-            h3: {
-              color: 'inherit',
-            },
-            h4: {
-              color: 'inherit',
-            },
-            'figure figcaption': {
-              color: 'inherit',
-            },
-            code: {
-              color: 'inherit',
-            },
-            'a code': {
-              color: 'inherit',
-            },
-            pre: {
-              color: 'inherit',
-              backgroundColor: 'transparent',
-            },
-            thead: {
-              color: 'inherit',
-              borderBottomColor: 'currentColor',
-            },
-            'tbody tr': {
-              borderBottomColor: 'currentColor',
-              opacity: '0.1',
-            },
-          },
+          900: '#181a1b',
         },
       },
+      fontFamily: {
+        sans: ['system-ui', 'sans-serif'],
+      },
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-body': theme('colors.neutral[900]'),
+            '--tw-prose-headings': theme('colors.neutral[950]'),
+            '--tw-prose-lead': theme('colors.neutral[600]'),
+            '--tw-prose-links': theme('colors.neutral[900]'),
+            '--tw-prose-bold': theme('colors.neutral[900]'),
+            '--tw-prose-counters': theme('colors.neutral[600]'),
+            '--tw-prose-bullets': theme('colors.neutral[400]'),
+            '--tw-prose-hr': theme('colors.neutral[200]'),
+            '--tw-prose-quotes': theme('colors.neutral[900]'),
+            '--tw-prose-quote-borders': theme('colors.neutral[200]'),
+            '--tw-prose-captions': theme('colors.neutral[600]'),
+            '--tw-prose-code': theme('colors.neutral[900]'),
+            '--tw-prose-pre-code': theme('colors.neutral[100]'),
+            '--tw-prose-pre-bg': theme('colors.neutral[800]'),
+            '--tw-prose-th-borders': theme('colors.neutral[300]'),
+            '--tw-prose-td-borders': theme('colors.neutral[200]'),
+            '--tw-prose-invert-body': theme('colors.neutral[100]'),
+            '--tw-prose-invert-headings': theme('colors.neutral[50]'),
+            '--tw-prose-invert-lead': theme('colors.neutral[400]'),
+            '--tw-prose-invert-links': theme('colors.neutral[100]'),
+            '--tw-prose-invert-bold': theme('colors.neutral[100]'),
+            '--tw-prose-invert-counters': theme('colors.neutral[400]'),
+            '--tw-prose-invert-bullets': theme('colors.neutral[600]'),
+            '--tw-prose-invert-hr': theme('colors.neutral[700]'),
+            '--tw-prose-invert-quotes': theme('colors.neutral[100]'),
+            '--tw-prose-invert-quote-borders': theme('colors.neutral[700]'),
+            '--tw-prose-invert-captions': theme('colors.neutral[400]'),
+            '--tw-prose-invert-code': theme('colors.neutral[100]'),
+            '--tw-prose-invert-pre-code': theme('colors.neutral[300]'),
+            '--tw-prose-invert-pre-bg': theme('colors.neutral[800]'),
+            '--tw-prose-invert-th-borders': theme('colors.neutral[600]'),
+            '--tw-prose-invert-td-borders': theme('colors.neutral[700]'),
+          },
+        },
+      }),
     },
   },
-  plugins: [
-    require("@tailwindcss/typography")
-  ],
+  plugins: [require('@tailwindcss/typography')],
   // Tối ưu hóa cho production
   future: {
     hoverOnlyWhenSupported: true,
   },
   experimental: {
     optimizeUniversalDefaults: true,
-  }
-}
+  },
+};
